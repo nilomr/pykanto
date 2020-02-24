6c8c26c8101fee24c6057609de99c35bc93fabda
nilomr <nilomerinorecalde@gmail.com>
2020-02-24 13:36:44 +0000

    first commit for gtit tests from wytham desktop

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Codeorded in Wytham Wooods. Oxford, UK',
+    author='Nilo M. Recalde',
+    license='MIT',
+)
