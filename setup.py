f3025bb5f4420d32622afab4550819e6a71ce53e
Nilo <60854434+nilomr@users.noreply.github.com>
2020-02-28 16:47:20 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Codeorded in Wytham Wooods. Oxford, UK',
+    description='Recorded in Wytham Wooods. Oxford, UK',
     author='Nilo M. Recalde',
     license='MIT',
 )
