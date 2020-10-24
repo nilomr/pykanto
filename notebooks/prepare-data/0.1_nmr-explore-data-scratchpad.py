f120736a1ea546a6904b7fdf8c6c4bbfc260c366
nilomr <nilomerinorecalde@gmail.com>
2020-10-24 19:22:24 +0100

    added cumulative plot and changed titles

--- notebooks/prepare-data/0.1_nmr-explore-data-scratchpad.py
+++ notebooks/prepare-data/0.1_nmr-explore-data-scratchpad.py
@@ -277,7 +277,7 @@ plt.close()
 # Plot frequency distribution of syllable counts and types
 
 nbins = 25
-fig = plt.figure(figsize=(10, 3))
+fig = plt.figure(figsize=(10, 4))
 gs = fig.add_gridspec(1, 2, width_ratios=[1, 1], hspace=0, wspace=0.15)
 axes = [fig.add_subplot(gs[i]) for i in range(2)]
 
@@ -293,13 +293,28 @@ for ax in axes:
     ax.spines["bottom"].set_visible(False)
     ax.spines["right"].set_visible(False)
     ax.spines["left"].set_visible(False)
+
     ax.tick_params(axis=u"both", which=u"both", length=0)
     for tick in ax.get_xaxis().get_major_ticks():
         tick.set_pad(8.0)
         tick.label1 = tick._get_text1()
+
 plt.yscale("log")
 
-fig.suptitle("Frequency distribution of note types and counts", fontsize=15, y=1.03)
+plt.yticks([1, 10, 100], ["1", "10", "100"])
+
+plt.text(
+    x=0.5,
+    y=0.88,
+    s="Frequency distribution of note types and counts",
+    fontsize=15,
+    fontweight="bold",
+    ha="center",
+    transform=fig.transFigure,
+)
+
+plt.subplots_adjust(top=0.8, wspace=0.3)
+
 axes[0].set_ylabel("Number of birds", labelpad=13, fontsize=11)
 axes[0].set_xlabel("Number of note types", labelpad=13, fontsize=11)
 axes[1].set_xlabel("Number of notes", labelpad=13, fontsize=11)
@@ -348,11 +363,10 @@ plt.close()
 # Plot syllable durations
 
 fig_dims = (10, 4)
-# sns.set_palette("cubehelix")
 fig, ax = plt.subplots(figsize=fig_dims)
-fig.suptitle("Note duration", fontsize=15, y=1.01)
 
 for indv in np.unique(syllable_df.indv):
+    plt.xlim(0, 0.4)
     plot = sns.distplot(
         (
             syllable_df[syllable_df.indv == indv]["end_time"]
@@ -372,18 +386,44 @@ ax.spines["bottom"].set_visible(False)
 ax.spines["right"].set_visible(False)
 ax.spines["left"].set_visible(False)
 ax.get_legend().set_visible(False)
+
+plt.xticks(
+    np.arange(0, 0.45, 0.05),
+    ["0", "0.05", "0.1", "0.15", "0.2", "0.25", "0.3", "0.35", "0.4"],
+)
+
 ax.tick_params(axis=u"both", which=u"both", length=0)
 for tick in ax.get_xaxis().get_major_ticks():
     tick.set_pad(8.0)
     tick.label1 = tick._get_text1()
 
+plt.text(
+    x=0.5,
+    y=0.94,
+    s="Distribution of note durations",
+    fontsize=15,
+    fontweight="bold",
+    ha="center",
+    transform=fig.transFigure,
+)
+plt.text(
+    x=0.5,
+    y=0.88,
+    s="Each line corresponds to a different bird",
+    fontsize=11,
+    color="#575757",
+    ha="center",
+    transform=fig.transFigure,
+)
+
+plt.subplots_adjust(top=0.8, wspace=0.3)
 plot.set_xlabel("Duration (s)", fontsize=11, labelpad=13)
 plot.set_ylabel("Density", fontsize=11, labelpad=13)
 plot.tick_params(labelsize=11)
 
 plt.annotate(
     "n = {}".format(len(np.unique(syllable_df.indv))),
-    xy=(0.03, 0.9),
+    xy=(0.91, 0.9),
     xycoords="axes fraction",
 )
 
@@ -423,6 +463,86 @@ times_of_day = song_datetimes["hour"].value_counts()
 
 sns.distplot(song_datetimes["hour"], kde=False, bins=20, kde_kws={"bw": 0.4})
 
+#%%
+
+# Plot time of day per song
+
+fig_dims = (10, 4)
+fig, ax = plt.subplots(figsize=fig_dims)
+
+for nestbox in np.unique(song_datetimes.nestbox):
+
+    counts = (
+        song_datetimes[song_datetimes.nestbox == nestbox]["hour"]
+        .value_counts()
+        .rename_axis("time")
+        .reset_index(name="count")
+    )
+
+    plot = sns.lineplot(
+        x=counts["time"], y=counts["count"], label=False, color="#8ea0c9", ax=ax,
+    )
+
+
+plt.setp(plot.lines, alpha=0.6)
+ax.grid(False)
+ax.set_facecolor("#f2f1f0")
+ax.spines["top"].set_visible(False)
+ax.spines["bottom"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.spines["left"].set_visible(False)
+ax.get_legend().set_visible(False)
+# ax.set(yscale="log")
+
+
+ax.tick_params(axis=u"both", which=u"both", length=0)
+for tick in ax.get_xaxis().get_major_ticks():
+    tick.set_pad(8.0)
+    tick.label1 = tick._get_text1()
+
+plt.text(
+    x=0.5,
+    y=0.94,
+    s="Number of songs recorded per hour",
+    fontsize=15,
+    fontweight="bold",
+    ha="center",
+    transform=fig.transFigure,
+)
+plt.text(
+    x=0.5,
+    y=0.88,
+    s="Each line corresponds to a different bird",
+    fontsize=11,
+    color="#575757",
+    ha="center",
+    transform=fig.transFigure,
+)
+
+plt.subplots_adjust(top=0.8, wspace=0.3)
+plot.set_xlabel("Time of the day (am)", fontsize=11, labelpad=13)
+plot.set_ylabel("Number of songs", fontsize=11, labelpad=13)
+plot.tick_params(labelsize=11)
+
+plt.annotate(
+    "n = {}".format(len(np.unique(syllable_df.indv))),
+    xy=(0.91, 0.9),
+    xycoords="axes fraction",
+)
+
+fig_out = (
+    FIGURE_DIR
+    / YEAR
+    / "population"
+    / ("time_plot" + str(datetime.now().strftime("%Y-%m-%d_%H:%M")) + ".png")
+)
+ensure_dir(fig_out)
+plt.savefig(
+    fig_out, dpi=300, bbox_inches="tight", pad_inches=0.3, transparent=False,
+)
+
+plt.close()
+
 
 # %%
 
@@ -430,9 +550,38 @@ sns.distplot(song_datetimes["hour"], kde=False, bins=20, kde_kws={"bw": 0.4})
 
 ## Build dictionary of syllable types per song
 
+## Parellelised code; only run in cluster
+
+
+def build_note_dictionary(empty_dict=song_dict, df=all_indv_dfs, metadata=metadata):
+    song_loc = [
+        Path(song).name for song in metadata[metadata.nestbox == indv].wav_loc.tolist()
+    ]
+    song_dict[indv] = {}
+    song_n = 0
+    for song in song_loc:
+        song_n += 1
+        labels = all_indv_dfs[
+            (all_indv_dfs.indv == indv) & (all_indv_dfs.key == str(song))
+        ].hdbscan_labels.tolist()
+        song_dict[indv].update({song_n: labels})
+
+
 song_dict = {}
 
-for indv in np.unique(all_indv_dfs.indv)[:3]:
+with Parallel(n_jobs=-2, verbose=3) as parallel:
+    parallel(
+        delayed(build_note_dictionary)(
+            empty_dict=song_dict, df=all_indv_dfs, metadata=metadata
+        )
+        for indv in np.unique(all_indv_dfs.indv)
+    )
+
+# %%
+
+song_dict = {}
+
+for indv in np.unique(all_indv_dfs.indv):
     song_loc = [
         Path(song).name for song in metadata[metadata.nestbox == indv].wav_loc.tolist()
     ]
@@ -447,78 +596,63 @@ for indv in np.unique(all_indv_dfs.indv)[:3]:
 
         song_dict[indv].update({song_n: labels})
 
-#%%
-
-# pd.DataFrame.from_dict(song_dict)
-
-# pd.concat({k: pd.DataFrame(v).T for k, v in song_dict.items()}, axis=0)
-
-pd.DataFrame.from_dict(
-    {(i, j): song_dict[i][j] for i in song_dict.keys() for j in song_dict[i].keys()},
-    orient="index",
-)
-
 
 #%%
 
+## Cumulative new songs
+
+data = []
 
 for bird, songs in song_dict.items():
+    new = 0
     for number, labels in songs.items():
         new_list = []
-        for label in labels:
-            for b_number, b_labels in song_dict[
-                bird
-            ].items():  # the rest of songs for the current bird
-                if b_number < number:
-                    if label in b_labels:
-                        new_list.append(0)
-                    else:
-                        new_list.append(1)
-        if 1 in new_list:
-            new = 1
 
-        print(bird, number, new)
+        for label in labels:
+
+            previous_labs = [
+                b_labels
+                for b_number, b_labels in song_dict[bird].items()
+                if b_number < number
+            ]  # the rest of songs for the current bird
+            previous_labs_flat = [item for sublist in previous_labs for item in sublist]
+
+            if label in previous_labs_flat:
+                new_list.append(0)
+            else:
+                new_list.append(1)
+
+        if 1 in new_list:
+            new += 1
+
+        temp_dict = {"bird": bird, "song_order": number, "cumulative_n": new}
+        data.append(dict(temp_dict))
+
+        # print(dict(zip(bird, number, new)))
 
         # [key for key in song_dict[bird].keys()][0:number-1]
 
-
-# song_dict.values()
-
-
-#%%
-
-
-Path(metadata[metadata.nestbox == indv].wav_loc.tolist()[0]).name
-
-# all_indv_dfs[all_indv_dfs.indv == 'B11']
+cumulative_df = pd.DataFrame(data)
 
 
 #%%
 
-for indv in np.unique(all_indv_dfs.indv):
-    print(indv)
+## Plot cumulative 'curves'
 
-#%%
-
-
-fig_dims = (10, 4)
-# sns.set_palette("cubehelix")
+fig_dims = (8, 4)
 fig, ax = plt.subplots(figsize=fig_dims)
-fig.suptitle("Note duration", fontsize=15, y=1.01)
 
-for indv in np.unique(syllable_df.indv):
-    plot = sns.distplot(
-        (
-            syllable_df[syllable_df.indv == indv]["end_time"]
-            - syllable_df[syllable_df.indv == indv]["start_time"]
-        ),
-        label=False,
-        rug=False,
-        hist=False,
-        kde_kws={"alpha": 0.3, "color": "#8ea0c9", "clip": (0.0, 0.4)},
-        ax=ax,
+
+for bird in np.unique(cumulative_df.bird):
+    x_data = cumulative_df[cumulative_df.bird == bird]["song_order"]
+    y_data = cumulative_df[cumulative_df.bird == bird]["cumulative_n"]
+    # y_data_norm = (y_data - np.min(y_data)) / (np.max(y_data) - np.min(y_data))
+
+    plot = sns.lineplot(
+        x=x_data, y=y_data, label=False, color="#8ea0c9", linewidth=4, ax=ax,
     )
 
+# ax.set(xscale="log")
 ax.grid(False)
 ax.set_facecolor("#f2f1f0")
 ax.spines["top"].set_visible(False)
@@ -526,33 +660,55 @@ ax.spines["bottom"].set_visible(False)
 ax.spines["right"].set_visible(False)
 ax.spines["left"].set_visible(False)
 ax.get_legend().set_visible(False)
+
+ax.xaxis.set_ticks(np.arange(0, 1100, 100))
+
+
 ax.tick_params(axis=u"both", which=u"both", length=0)
 for tick in ax.get_xaxis().get_major_ticks():
     tick.set_pad(8.0)
     tick.label1 = tick._get_text1()
 
-plot.set_xlabel("Duration (s)", fontsize=11, labelpad=13)
-plot.set_ylabel("Density", fontsize=11, labelpad=13)
+
+plt.setp(plot.lines, alpha=0.1)
+plot.set_xlabel("Number of songs recorded", fontsize=11, labelpad=13)
+plot.set_ylabel("New songs", fontsize=11, labelpad=13)
 plot.tick_params(labelsize=11)
 
+
+plt.text(
+    x=0.5,
+    y=0.94,
+    s="Cumulative new song types",
+    fontsize=15,
+    fontweight="bold",
+    ha="center",
+    transform=fig.transFigure,
+)
+plt.text(
+    x=0.5,
+    y=0.88,
+    s="Each line corresponds to a different bird",
+    fontsize=11,
+    color="#575757",
+    ha="center",
+    transform=fig.transFigure,
+)
+
+plt.subplots_adjust(top=0.8, wspace=0.3)
+
 plt.annotate(
-    "n = {}".format(len(np.unique(syllable_df.indv))),
-    xy=(0.03, 0.9),
+    "n = {}".format(len(np.unique(cumulative_df.bird))),
+    xy=(0.9, 0.93),
     xycoords="axes fraction",
 )
 
-
-# plt.show()
 
 fig_out = (
     FIGURE_DIR
     / YEAR
     / "population"
-    / (
-        "syllable_duration_pd_"
-        + str(datetime.now().strftime("%Y-%m-%d_%H:%M"))
-        + ".png"
-    )
+    / ("cumulative_plot" + str(datetime.now().strftime("%Y-%m-%d_%H:%M")) + ".png")
 )
 ensure_dir(fig_out)
 plt.savefig(
@@ -560,3 +716,5 @@ plt.savefig(
 )
 
 plt.close()
+
+# %%
