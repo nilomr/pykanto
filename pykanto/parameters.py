754c1b27406be7d6700bd8a3cdda4c3ab0dbca37
nilomr <nilomerinorecalde@gmail.com>
2022-01-25 18:13:41 +0000

    adds type hints to params

--- pykanto/parameters.py
+++ pykanto/parameters.py
@@ -1,10 +1,13 @@
-# ─── DESCRIPTION ────────────────────────────────────────────────────────────────
-# Parameter class - to keep all settings in one place
+# ─── DESCRIPTION ─────────────────────────────────────────────────────────────
+"""
 
+"""
+# ──── IMPORTS ────────────────────────────────────────────────────────────────
 import json
-from typing import Any
-
+from typing import Any, Tuple
 from pykanto.utils.compute import print_dict
+
+# ──── CLASSES AND METHODS ────────────────────────────────────────────────────
 
 
 class Parameters():
@@ -21,60 +24,62 @@ class Parameters():
         # Set defaults first
 
         # Spectrogramming
-        self.window_length = 1024
+        self.window_length: int = 1024
         """Number of samples. Defaults to 1024."""
-        self.hop_length = 128
+        self.hop_length: int = 128
         """Number of samples between successive frames. Defaults to 128."""
-        self.fft_size = 1024
+        self.fft_size: int = 1024
         """Number of bins used to divide the window. Defaults to 1024."""
-        self.num_mel_bins = 224
+        self.num_mel_bins: int = 224
         """Number of Mel frequency bins to use 
         (n of 'rows' in mel-spectrogram). Defaults to 224."""
-        self.sr = 22050
+        self.sr: int = 22050
         """The sampling rate (samples taken per second). 
         Defaults to 22050."""
-        self.hop_length_ms = self.sr / self.hop_length
+        self.hop_length_ms: float = self.sr / self.hop_length
         """Calculated automatically."""
-        self.fft_rate = 1000 / self.hop_length_ms
+        self.fft_rate: float = 1000 / self.hop_length_ms
         """Calculated automatically."""
-        self.top_dB = 65
+        self.top_dB: int = 65
         """Top dB to keep. Defaults to 65.
         Example: if ``top_dB`` = 65 anything below -65dB will be masked."""
-        self.lowcut = 100
-        """Minimum frequency (in Hz) to include by default. Defaults to 100."""
-        self.highcut = 1000
+        self.lowcut: int = 1000
+        """Minimum frequency (in Hz) to include by default. Defaults to 1000."""
+        self.highcut: int = 10000
         """Maximum frequency (in Hz) to include by default. 
-        Defaults to 1000."""
+        Defaults to 10000."""
         self.dereverb: bool = False
+        """Wether to reduce reverberation in the spectrograms. 
+        Defaults to False"""
 
         # Segmentation
-        self.max_dB = -30
+        self.max_dB: int = -30
         """Maximum threshold to reach during segmentation, in dB. 
         Defaults to -30"""
-        self.dB_delta = 5
+        self.dB_delta: int = 5
         """Size of thresholding steps, in dB. Defaults to 5."""
-        self.silence_threshold = 0.2
+        self.silence_threshold: float = 0.2
         """Threshold separating silence from voiced segments. 
         Between 0.1 and 0.3 tends to work well. Defaults to 0.2."""
-        self.max_unit_length = 0.4
+        self.max_unit_length: float = 0.4
         """Maximum unit length allowed. Defaults to 0.4. """
-        self.min_unit_length = 0.03
+        self.min_unit_length: float = 0.03
         """Minimum unit length allowed. Defaults to 0.03."""
-        self.min_silence_length = 0.001
+        self.min_silence_length: float = 0.001
         """Minimum silence length allowed. Defaults to 0.001."""
-        self.gauss_sigma = 3
+        self.gauss_sigma: int = 3
         """Sigma for gaussian kernel. Defaults to 3."""
 
         # general settings
-        self.song_level = False
+        self.song_level: bool = False
         """Whether to return the average of all units.
         Defaults to False (return individual units in each vocalisation)"""
-        self.subset = (0, -1)
+        self.subset: Tuple[int, int] = (0, -1)
         """Indices of the first and last items to include in the dataset. 
         Defaults to (0,-1); full dataset"""
-        self.n_jobs = -2
+        self.n_jobs: int = -2
         """How many jobs to create. Default is available number of cores -2."""
-        self.verbose = True
+        self.verbose: bool = True
         """How much detail to provide. Defaults to True (verbose)."""
         # Update with user-provided parameters.
         self.__dict__.update(kwargs)
