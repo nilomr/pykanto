611c644fb8cad1449d15b4c5dbbe934f55377d25
nilomr <nilomerinorecalde@gmail.com>
2022-05-02 11:10:30 +0100

    testing only

--- tests/scratchpad.py
+++ tests/scratchpad.py
@@ -14,6 +14,7 @@
 # %%──── LIBRARIES ─────────────────────────────────────────────────────────────
 
 from __future__ import annotations
+from bokeh.palettes import Set3_12
 
 import json
 import os
@@ -25,7 +26,6 @@ from pathlib import Path
 from typing import Any, Dict, List
 from xml.etree import ElementTree
 
-import audio_metadata
 import git
 import librosa
 import matplotlib.pyplot as plt
@@ -153,3 +153,95 @@ for path in to_rm:
     if path.exists():
         shutil.rmtree(str(path))
 assert all([f.exists() for f in to_rm]) == False
+
+
+# ──── INTERACTIVE APP DEMO ────────────────────────────────────────────────────
+
+DATASET_ID = 'LABEL_APP_DEMO'
+PROJECT_ROOT = Path('/home/nilomr/projects/great-tit-song')
+RAW_DATA = PROJECT_ROOT / 'data' / 'raw' / 'LABEL_APP_DEMO_DATA'
+DIRS = ProjDirs(PROJECT_ROOT, RAW_DATA, mkdir=True)
+print(DIRS)
+
+
+wav_filepaths, xml_filepaths = [get_file_paths(
+    DIRS.RAW_DATA, [ext]) for ext in ['.WAV', '.xml']]
+files_to_segment = get_wavs_w_annotation(wav_filepaths, xml_filepaths)
+
+
+segment_files_parallel(
+    files_to_segment,
+    DIRS,
+    resample=22050,
+    parser_func=parse_sonic_visualiser_xml,
+    min_duration=.1,
+    min_freqrange=100,
+    labels_to_ignore=["NOISE"]
+)
+
+
+# Define parameters
+params = Parameters(
+    # Spectrogramming
+    window_length=1024,
+    hop_length=128,
+    n_fft=1024,
+    num_mel_bins=224,
+    sr=22050,
+    top_dB=65,                  # top dB to keep
+    lowcut=2000,
+    highcut=10000,
+    # Segmentation,
+    max_dB=-30,                 # Max threshold for segmentation
+    dB_delta=5,                 # n thresholding steps, in dB
+    silence_threshold=0.1,      # Between 0.1 and 0.3 tends to work
+    max_unit_length=0.4,        # Maximum unit length allowed
+    min_unit_length=0.02,       # Minimum unit length allowed
+    min_silence_length=0.001,   # Minimum silence length allowed
+    gauss_sigma=3,              # Sigma for gaussian kernel
+    # general settings
+    song_level=True,
+    subset=None,
+    verbose=False,
+    num_cpus=None,
+)
+
+# np.random.seed(123)
+# random.seed(123)
+dataset = SongDataset(
+    DATASET_ID, DIRS, parameters=params, overwrite_dataset=True,
+    random_subset=None, overwrite_data=False)
+
+
+dataset.segment_into_units()
+dataset.vocs['ID'] = 'TR43633'
+
+dataset.get_units()
+dataset.cluster_ids(min_sample=20)
+dataset.prepare_interactive_data()
+
+
+out_dir = DIRS.DATA / "datasets" / DATASET_ID / f"{DATASET_ID}.db"
+dataset = pickle.load(open(out_dir, "rb"))
+
+dataset.open_label_app()
+
+for song_level in [True, False]:
+    dataset.parameters.update(song_level=song_level)
+    dataset.get_units()
+
+dataset.reload()
+for song_level in [True, False]:
+    dataset.parameters.update(song_level=song_level)
+    dataset.cluster_ids(min_sample=5)
+
+for song_level in [True, False]:
+    dataset.parameters.update(song_level=song_level)
+    dataset.prepare_interactive_data()
+
+dataset.parameters.update(song_level=True)
+
+pal = list(Set3_12)
+pal.append('#ffed6f')
+
+dataset.open_label_app()
