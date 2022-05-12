5ecf7177a505f7e051d41ec0c7b9fd500cd6c414
nilomr <nilomerinorecalde@gmail.com>
2022-05-12 16:39:34 +0100

    add method to save annotations to original json

--- pykanto/utils/write.py
+++ pykanto/utils/write.py
@@ -15,12 +15,18 @@ import os.path
 import shutil
 import sys
 import tarfile
+import uuid
 from pathlib import Path
-from typing import Dict, List
+from typing import TYPE_CHECKING, Dict, List
 
 import numpy as np
 import ujson
 from tqdm import tqdm
+
+if TYPE_CHECKING:
+    from pykanto.dataset import KantoData
+
+from pykanto.utils.compute import with_pbar
 
 # ──── FUNCTIONS ───────────────────────────────────────────────────────────────
 
@@ -88,6 +94,59 @@ def save_json(json_object: Dict, json_loc: Path) -> None:
     """
     with open(json_loc, "w", encoding="utf-8") as f:
         ujson.dump(json_object, f, ensure_ascii=False, indent=4)
+
+
+# Save segmentation and label data to jsons:
+def save_to_jsons(dataset: KantoData) -> None:
+    """
+    Appends new metadata generated in pykanto to the original json metadata
+    files that were used to create a `KantoData` dataset. These usually include
+    things like type labels and unit oset/offsets.
+
+    Args:
+        dataset (KantoData): Dataset object.
+    """
+    # need to convert numpy to list before can dump to json:
+    df = dataset.vocs.copy()
+
+    # Convert any numpy arrays to lists, pathlib.Paths to str
+    arraycols, pathcols = [
+        [col for col in df.columns if isinstance(df[col][0], coltype)]
+        for coltype in [np.ndarray, Path]
+    ]
+    df[arraycols] = df[arraycols].applymap(lambda x: x.tolist())
+
+    # Convert any pathlib.Paths to str
+    arraycols = [col for col in df.columns if isinstance(df[col][0], Path)]
+    df[pathcols] = df[pathcols].applymap(lambda x: x.as_posix())
+
+    # Parallelisable part
+    # TODO: refactor and parallelise with ray
+    for jsonfile in with_pbar(
+        dataset.DIRS.JSON_LIST,
+        desc="Adding new metadata to .json files "
+        f" in {dataset.DIRS.JSON_LIST[0].parent}",
+    ):
+        idx = jsonfile.stem
+
+        if idx in dataset.vocs.index:
+            dfdict = df.loc[idx].to_dict()
+            with open(jsonfile, "r") as f:
+                data = json.load(f)
+                for key in dfdict:
+                    if key not in data:
+                        data[key] = dfdict[key]
+
+            # create randomly named temporary file to avoid
+            # interference with other thread/asynchronous request
+            tempfile = os.path.join(
+                os.path.dirname(jsonfile), str(uuid.uuid4())
+            )
+            with open(tempfile, "w") as f:
+                json.dump(data, f, indent=4)
+
+            # rename temporary file replacing old file
+            os.replace(tempfile, jsonfile)
 
 
 class NumpyEncoder(json.JSONEncoder):
