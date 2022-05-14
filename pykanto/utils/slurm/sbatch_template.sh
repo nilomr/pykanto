1b4882a3cecd48c1594052510abffb83a571369c
nilomr <nilomerinorecalde@gmail.com>
2022-05-14 17:46:09 +0100

    Revert "load anaconda3 module by default, not user env"

--- pykanto/utils/slurm/sbatch_template.sh
+++ pykanto/utils/slurm/sbatch_template.sh
@@ -27,6 +27,10 @@ module load Anaconda3
 export CONPREFIX=$DATA/envs/greti-env
 source activate $CONPREFIX
 
+# module load Anaconda3
+# export CONPREFIX=$DATA/envs/greti-env
+# source activate $CONPREFIX
+
 echo $CUDA_VISIBLE_DEVICES
 nvidia-smi
 
