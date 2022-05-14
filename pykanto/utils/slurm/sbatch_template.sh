321aa773933b86562be7b0ffa44a18b12ace74bd
nilomr <nilomerinorecalde@gmail.com>
2022-05-14 16:48:23 +0100

    load anaconda3 module by default, not user env

--- pykanto/utils/slurm/sbatch_template.sh
+++ pykanto/utils/slurm/sbatch_template.sh
@@ -20,11 +20,9 @@
 # Load modules or your own conda environment here
 # module load pytorch/v1.4.0-gpu
 # conda activate {{CONDA_ENV}}
-{{LOAD_ENV}}
 
-# module load Anaconda3
-# export CONPREFIX=$DATA/envs/greti-env
-# source activate $CONPREFIX
+module load Anaconda3
+{{LOAD_ENV}}
 
 echo $CUDA_VISIBLE_DEVICES
 nvidia-smi
