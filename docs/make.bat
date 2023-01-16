2228fa741ea0c605de355051f5f19f6b90178aa2
nilomr <nilomerinorecalde@gmail.com>
2023-01-16 15:54:48 +0000

    update makefile

--- docs/make.bat
+++ docs/make.bat
@@ -8,9 +8,7 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set SOURCEDIR=.
-set BUILDDIR=.
-
-if "%1" == "" goto help
+set BUILDDIR=_build
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
@@ -24,6 +22,8 @@ if errorlevel 9009 (
 	echo.https://www.sphinx-doc.org/
 	exit /b 1
 )
+
+if "%1" == "" goto help
 
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
